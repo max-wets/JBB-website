7f558dd150c951e34b5ea5ac7689062bb33d5823
Maxime Wets <wetsmaxime@gmail.com>
2022-01-09 15:29:01 +0100

    created view selector

--- src/components/products/ProductsList.tsx
+++ src/components/products/ProductsList.tsx
@@ -1,7 +1,7 @@
 import classes from "./ProductsList.module.css";
 import ProductItem from "./ProductItem";
 import Pagination from "../pagination/Pagination";
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Grid, GridItem, Flex } from "@chakra-ui/react";
 import { useEffect, useMemo } from "react";
 
 let PageSize = 12;
@@ -34,8 +34,31 @@ function ProductsList(props: { products; currentPage; setCurrentPage }) {
     return props.products.slice(firstPageIndex, lastPageIndex);
   }, [props.currentPage, props.products]);
 
+  function ViewSelector(props: { productsLength; pageSize }) {
+    const nbrOfViews = Math.ceil(props.productsLength / props.pageSize);
+    const views = [];
+    for (let i = 1; i < nbrOfViews + 1; i++) {
+      views.push(i * props.pageSize);
+    }
+    return (
+      <ul className={classes.resultcount}>
+        <li className={classes.viewtitle}>Produits par page:</li>
+        {views.map((view) => (
+          <li>{view}</li>
+        ))}
+        <li>Tous</li>
+      </ul>
+    );
+  }
+
   return (
     <div className={classes.productlist}>
+      <Flex flexDirection="row-reverse">
+        <ViewSelector
+          productsLength={props.products.length}
+          pageSize={PageSize}
+        />
+      </Flex>
       <Grid templateColumns="repeat(3, 1fr)" gap={6}>
         {currentData.map((product) => (
           <GridItem w="100%" bg="gray.200">
