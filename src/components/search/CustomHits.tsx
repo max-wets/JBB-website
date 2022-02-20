f091e02c50060e1bd7fccab6f95b23fe085367f8
Maxime Wets <wetsmaxime@gmail.com>
2022-02-20 19:22:23 +0100

    search page UI done

--- src/components/search/CustomHits.tsx
+++ src/components/search/CustomHits.tsx
@@ -1,8 +1,62 @@
+import Image from "next/image";
+import Link from "next/link";
 import { connectStateResults } from "react-instantsearch-dom";
+import classes from "./CustomHits.module.css";
+import Pagination from "../pagination/Pagination";
+import { useMemo, useState } from "react";
+
+let PageSize = 3;
 
 function Hits({ searchState, searchResults }) {
   const validQuery = searchState.query?.length >= 1;
   console.log(searchResults?.hits);
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const currentData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return searchResults?.hits.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage, searchResults]);
+
+  function ResultHit({ id, title, description, imageUrl }) {
+    return (
+      <article key={id} className={classes.resulthit}>
+        <div className={classes.ctr}>
+          <div className={classes.thumbnail}>
+            <Link href={`/blog/${id}`}>
+              <a>
+                <Image
+                  width={120}
+                  height={120}
+                  alt={title}
+                  src={imageUrl}
+                  objectFit="fill"
+                />
+              </a>
+            </Link>
+          </div>
+          <div className={classes.content}>
+            <header className={classes.contentheader}>
+              <h2>
+                <Link href={`/blog/${id}`}>
+                  <a>{title}</a>
+                </Link>
+              </h2>
+            </header>
+            <div className={classes.contentsummary}>
+              <p>{description.slice(0, 200) + "..."}</p>
+            </div>
+            <div className={classes.readmore}>
+              <Link href={`/blog/${id}`}>
+                <a>Lire</a>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </article>
+    );
+  }
 
   return (
     <>
@@ -10,11 +64,27 @@ function Hits({ searchState, searchResults }) {
         <p>Aw snap! No search results were found.</p>
       )}
       {searchResults?.hits.length > 0 && validQuery && (
-        <ul>
-          {searchResults.hits.map((hit) => (
-            <li key={hit.id}>{hit.title}</li>
-          ))}
-        </ul>
+        <>
+          <div>
+            <ul>
+              {currentData.map((hit) => (
+                <ResultHit
+                  id={hit.id}
+                  title={hit.title}
+                  description={hit.description}
+                  imageUrl={hit.imageUrl}
+                />
+              ))}
+            </ul>
+          </div>
+          <Pagination
+            className={classes.paginationbar}
+            currentPage={currentPage}
+            totalCount={searchResults.hits.length}
+            pageSize={PageSize}
+            onPageChange={(page) => setCurrentPage(page)}
+          />
+        </>
       )}
     </>
   );
--- src/components/search/CustomSearchBox.tsx
+++ src/components/search/CustomSearchBox.tsx
@@ -1,16 +1,24 @@
 import classes from "./CustomSearchBox.module.css";
 import { connectSearchBox } from "react-instantsearch-dom";
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
 
 function SearchBox({ refine }) {
   return (
     <div className={classes.searchbox}>
       <form action="" role="search">
-        <input
-          className={classes.searchboxinput}
-          type="search"
-          placeholder="Chercher un article..."
-          onChange={(e) => refine(e.currentTarget.value)}
-        />
+        <InputGroup display="flex" alignItems="center" justifyContent="center">
+          <InputLeftElement
+            pointerEvents="none"
+            children={<SearchIcon size="sm" color="gray.300" />}
+          />
+          <input
+            className={classes.searchboxinput}
+            type="search"
+            placeholder="Chercher un article..."
+            onChange={(e) => refine(e.currentTarget.value)}
+          />
+        </InputGroup>
       </form>
     </div>
   );
