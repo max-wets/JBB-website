771a12cd291c8f3451a6407bf5443e2d615e2e17
Maxime Wets <wetsmaxime@gmail.com>
2022-04-10 18:19:18 +0200

    completed legal notice page

---
+++ src/pages/mentions-legales/index.tsx
@@ -0,0 +1,11 @@
+import LegalNoticeComponent from "../../components/legal-notice/LegalNoticeComponent";
+
+function LegalNoticePage() {
+  return (
+    <>
+      <LegalNoticeComponent />
+    </>
+  );
+}
+
+export default LegalNoticePage;
