ce1cad7323fc67222a589cacad380545e1251d0d
Maxime Wets <wetsmaxime@gmail.com>
2025-04-06 18:53:12 +0200

    chore(): refactored blog post fetching service

--- src/pages/blog/[blogArticleId]/index.tsx
+++ src/pages/blog/[blogArticleId]/index.tsx
@@ -81,57 +81,20 @@ export default function BlogDetailPage({
 export const getStaticProps: GetStaticProps = async ({
   params,
 }): Promise<GetStaticPropsResult<BlogDetailPageProps>> => {
-  const sortingFn = (a: BlogPostApi, b: BlogPostApi): number => {
-    const aDate = new Date(a.publishedAt);
-    const bDate = new Date(b.publishedAt);
-
-    if (aDate > bDate) {
-      return -1;
-    }
-    if (aDate < bDate) {
-      return 1;
-    }
-    return 0;
-  };
-
   const blogPostIdArr = (params!.blogArticleId as string).split("-");
   const blogPostDocumentId = blogPostIdArr[blogPostIdArr.length - 1];
-  const res = await axios.get<ApiResponse<BlogPostApi>>(
-    `${process.env.NEXT_PUBLIC_API_URL}/articles?populate=%2A&pagination[pageSize]=100&sort[0]=createdAt%3Adesc`
-  );
-  const data = res.data.data.sort(sortingFn);
-  const article = data.find(
-    (article) => article.documentId === blogPostDocumentId
-  );
-  if (!article)
-    throw new Error(
-      `Article with Document ID '${blogPostDocumentId}' not found`
-    );
-
-  const formatData = (post: BlogPostApi): BlogPost => {
-    return {
-      id: post.id.toString(),
-      documentId: post.documentId,
-      title: post.Name,
-      intro: post.Intro,
-      description: post.Description,
-      issueDate: post.updatedAt,
-      videoUrl: post.Video_URL,
-      imageUrl: post.Image.url,
-      categories: post.article_categories.map(
-        (category: CategoryApi) => category.Name
-      ),
-    };
-  };
+  const pageBlogPost = await getBlogPagePostDetails(blogPostDocumentId);
 
   return {
     props: {
-      article: formatData(article),
-      recentArticles: await getRecentBlogPosts(article.documentId),
-      prevNextPosts: await getPreviousAndNextBlogPosts(article.updatedAt),
-      recommendedArticles:
-        await getRecommendedBlogPostsWithSameCategories(article),
-      articleComments: await getBlogPostComments(article.id),
+      article: pageBlogPost,
+      recentArticles: await getRecentBlogPosts(pageBlogPost.documentId),
+      prevNextPosts: await getPreviousAndNextBlogPosts(pageBlogPost.issueDate),
+      recommendedArticles: await getRecommendedBlogPostsWithSameCategories(
+        pageBlogPost.documentId,
+        pageBlogPost.categories
+      ),
+      articleComments: await getBlogPostComments(Number(pageBlogPost.id)),
     },
     revalidate: 3600,
   };
@@ -152,24 +115,64 @@ export const getStaticPaths: GetStaticPaths = async () => {
   return { paths, fallback: true };
 };
 
+const getBlogPagePostDetails = async (
+  documentID: string
+): Promise<BlogPost> => {
+  const query = qs.stringify(
+    {
+      populate: ["Image", "article_categories"],
+      filters: {
+        documentId: {
+          $eq: documentID,
+        },
+      },
+      sort: ["updatedAt:desc"],
+      pagination: {
+        pageSize: 1,
+        page: 1,
+      },
+    },
+    {
+      encodeValuesOnly: true,
+    }
+  );
+  const { data } = await axios.get<ApiResponse<BlogPostApi>>(
+    `${process.env.NEXT_PUBLIC_API_URL}/articles?${query}`
+  );
+  if (!data || data.data.length < 1)
+    throw new Error("No Blog post was found with document ID: " + documentID);
+  const blogPostApi = data.data[0];
+  return {
+    id: blogPostApi.id,
+    documentId: blogPostApi.documentId,
+    intro: blogPostApi.Intro,
+    title: blogPostApi.Name,
+    description: blogPostApi.Description,
+    issueDate: blogPostApi.updatedAt,
+    imageUrl: blogPostApi.Image ? blogPostApi.Image.url : null,
+    videoUrl: blogPostApi.Video_URL,
+    categories: blogPostApi.article_categories.map(
+      (category: CategoryApi) => category.Name
+    ),
+  };
+};
+
 const getRecommendedBlogPostsWithSameCategories = async (
-  currentBlogPost: BlogPostApi,
+  currentBlogPostDocumentId: string,
+  currentBlogPostCategories: string[],
   count: number = 3
 ): Promise<BlogPost[]> => {
   try {
-    const blogPostCategories = currentBlogPost.article_categories.map(
-      (category) => category.Name
-    );
     const query = qs.stringify(
       {
         populate: ["Image", "article_categories"],
         filters: {
           documentId: {
-            $ne: currentBlogPost.documentId,
+            $ne: currentBlogPostDocumentId,
           },
           article_categories: {
             Name: {
-              in: blogPostCategories,
+              in: currentBlogPostCategories,
             },
           },
         },
@@ -305,7 +308,20 @@ const getBlogPostComments = async (
   blogPostID: number
 ): Promise<PostComment[]> => {
   try {
-    const url = `${process.env.NEXT_PUBLIC_API_URL}/comments-full?filters[ArticleID][$eq]=${blogPostID}&sort=updatedAt%3Adesc`;
+    const query = qs.stringify(
+      {
+        filters: {
+          ArticleID: {
+            $eq: blogPostID,
+          },
+        },
+        sort: ["updatedAt:desc"],
+      },
+      {
+        encodeValuesOnly: true,
+      }
+    );
+    const url = `${process.env.NEXT_PUBLIC_API_URL}/comments-full?${query}`;
     const { data } = await axios.get<PostCommentApi[]>(url);
     if (!data || data.length < 1) return [];
     return data.map((comment) => ({
